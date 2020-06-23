dc9ac7cdf95bcbc8872df77d7e2a36407a54b472
Sondre Kongsgård <sondre.dev@gmail.com>
2020-06-23 10:04:36 +0200

    Use improved weight function

--- src/Open3D/Geometry/TriangleMeshHoleFilling.cpp
+++ src/Open3D/Geometry/TriangleMeshHoleFilling.cpp
@@ -33,10 +33,36 @@
 #include <cmath>
 #include <iostream>
 #include <limits>
+#include <set>
 #include <string>
 
 namespace open3d {
 namespace geometry {
+
+class Weight {
+public:
+    Weight(const double cosine_of_max_dihedral_angle, const double area)
+        : cosine_of_max_dihedral_angle_(cosine_of_max_dihedral_angle),
+          area_(area) {}
+
+    Weight operator+(const Weight &w) {
+        return Weight(this->cosine_of_max_dihedral_angle_ +
+                              w.cosine_of_max_dihedral_angle_,
+                      this->area_ + w.area_);
+    }
+
+    bool operator<(const Weight &w) {
+        return this->cosine_of_max_dihedral_angle_ <
+                       w.cosine_of_max_dihedral_angle_ ||
+               (this->cosine_of_max_dihedral_angle_ ==
+                        w.cosine_of_max_dihedral_angle_ &&
+                this->area_ < w.area_);
+    }
+
+private:
+    double cosine_of_max_dihedral_angle_;
+    double area_;
+};
 
 class HoleFilling {
 public:
@@ -51,73 +77,68 @@ public:
         mesh_->RemoveDuplicatedTriangles();
         mesh_->RemoveUnreferencedVertices();
         mesh_->RemoveDegenerateTriangles();
-
-        // Starting index for the triangles to be added in the triangulation
-        // step
-        first_unrefined_triangle_idx_ = mesh_->triangles_.size();
     }
 
-    void IdentifyHoles() {
-        // Assume that the mesh is oriented, manifold, and connected
-        auto het_mesh = HalfEdgeTriangleMesh::CreateFromTriangleMesh(*mesh_);
-
-        if (het_mesh->IsEmpty()) {
-            utility::LogWarning("The mesh is non manifold\n");
-            return;
-        }
-
-        boundaries_ = het_mesh->GetBoundaries();
-        utility::LogDebug("Number of holes: {}\n",
-                          std::to_string(boundaries_.size()));
-    }
-
-    void TriangulateHoles() {
-        for (std::vector<int> hole : boundaries_) {
-            int n = int(hole.size());
-            std::vector<std::vector<float>> minimum_weight(
-                    n, std::vector<float>(n, 0));
-            std::vector<std::vector<int>> minimum_weight_index(
-                    n, std::vector<int>(n, -1));
-
-            std::vector<std::vector<std::tuple<float, float>>> minimum_weight2(
-                    n, std::vector<std::tuple<float, float>>(
-                               n, std::tuple<float, float>(0, 0)));
-
-            for (size_t j = 2; j < n; j++) {
-                for (size_t i = 0; i < n - j; i++) {
-                    float weight_min = std::numeric_limits<float>::max();
-                    int weight_index = -1;
-                    int k = i + j;
-
-                    for (size_t m = i + 1; m < k; m++) {
-                        float weight = minimum_weight[i][m] +
-                                       minimum_weight[m][k] +
-                                       TriangleMesh::ComputeTriangleArea(
-                                               mesh_->vertices_[i],
-                                               mesh_->vertices_[m],
-                                               mesh_->vertices_[k]);
-                        if (weight < weight_min) {
-                            weight_min = weight;
-                            weight_index = m;
-                        }
-                    }
-
-                    minimum_weight[j][k] = weight_min;
-                    minimum_weight_index[i][k] = weight_index;
-                }
-            }
-
-            AddMinimumWeightTriangulationToMesh(hole, minimum_weight_index, 0,
-                                                n - 1);
-        }
-    }
-
-    double CalculateCosineOfDihedralAngle(const Eigen::Vector3d &p0,
-                                          const Eigen::Vector3d &p1,
-                                          const Eigen::Vector3d &p2) {
-        const Eigen::Vector3d n1 = ((p2 - p1).cross(p0 - p1)).normalized();
-        const Eigen::Vector3d n2 = ((p2 - p1).cross(p0 - p1)).normalized();
+    double ComputeCosineOfDihedralAngle(
+            std::vector<Eigen::Vector3d> &first_point_set,
+            std::vector<Eigen::Vector3d> &second_point_set) {
+        const Eigen::Vector3d n1 =
+                ((first_point_set[2] - first_point_set[1])
+                         .cross(first_point_set[0] - first_point_set[1]))
+                        .normalized();
+        const Eigen::Vector3d n2 =
+                ((second_point_set[2] - second_point_set[1])
+                         .cross(second_point_set[0] - second_point_set[1]))
+                        .normalized();
         return n1.dot(n2);
+    }
+
+    double ComputeMaxCosineOfDihedralAngle(int vidx0, int vidx1, int vidx2) {
+        // Find the adjacent triangle indices
+        std::unordered_set<int> adjacent_triangle_indices(3);
+        auto edges_to_triangles = mesh_->GetEdgeToTrianglesMap();
+
+        for (auto tnb :
+             edges_to_triangles[mesh_->GetOrderedEdge(vidx0, vidx1)]) {
+            adjacent_triangle_indices.insert(tnb);
+        }
+        for (auto tnb :
+             edges_to_triangles[mesh_->GetOrderedEdge(vidx0, vidx2)]) {
+            adjacent_triangle_indices.insert(tnb);
+        }
+        for (auto tnb :
+             edges_to_triangles[mesh_->GetOrderedEdge(vidx1, vidx2)]) {
+            adjacent_triangle_indices.insert(tnb);
+        }
+
+        // Make a point set which spans the plane given by the three vertices
+        // vidx0, vidx1, and vidx2
+        std::vector<Eigen::Vector3d> first_point_set = {
+                mesh_->vertices_[vidx0], mesh_->vertices_[vidx1],
+                mesh_->vertices_[vidx2]};
+
+        // Find the cosine of the highest dihedral angle
+        double cosine_of_max_dihedral_angle =
+                std::numeric_limits<double>::lowest();
+        for (int tidx : adjacent_triangle_indices) {
+            const Eigen::Vector3i triangle = mesh_->triangles_[tidx];
+            std::vector<Eigen::Vector3d> second_point_set = {
+                    mesh_->vertices_[triangle[0]],
+                    mesh_->vertices_[triangle[1]],
+                    mesh_->vertices_[triangle[2]]};
+            cosine_of_max_dihedral_angle =
+                    std::max(cosine_of_max_dihedral_angle,
+                             ComputeCosineOfDihedralAngle(first_point_set,
+                                                          second_point_set));
+        }
+        return cosine_of_max_dihedral_angle;
+    }
+
+    Weight ComputeWeight(int vidx0, int vidx1, int vidx2) {
+        return Weight(ComputeMaxCosineOfDihedralAngle(vidx0, vidx1, vidx2),
+                      TriangleMesh::ComputeTriangleArea(
+                              mesh_->vertices_[vidx0], mesh_->vertices_[vidx1],
+                              mesh_->vertices_[vidx2]));
     }
 
     void AddMinimumWeightTriangulationToMesh(
@@ -141,84 +162,58 @@ public:
         }
     }
 
-    void RefineMesh() {
-        double density_control_factor = std::sqrt(2.0);
-        mesh_->ComputeAdjacencyList();
+    void IdentifyHoles() {
+        // Assume that the mesh is oriented, manifold, and connected
+        auto het_mesh = HalfEdgeTriangleMesh::CreateFromTriangleMesh(*mesh_);
 
+        if (het_mesh->IsEmpty()) {
+            utility::LogWarning("The mesh is non-manifold\n");
+            return;
+        }
+
+        boundaries_ = het_mesh->GetBoundaries();
+        utility::LogDebug("Number of holes: {}\n",
+                          std::to_string(boundaries_.size()));
+    }
+
+    void TriangulateHoles() {
         for (std::vector<int> hole : boundaries_) {
-            // int n = int(hole.size());
+            int size = int(hole.size());
+            std::vector<std::vector<Weight>> minimum_weight(
+                    size, std::vector<Weight>(size, Weight(0, 0)));
+            std::vector<std::vector<int>> minimum_weight_index(
+                    size, std::vector<int>(size, -1));
 
-            for (size_t tidx = first_unrefined_triangle_idx_;
-                 tidx < mesh_->triangles_.size(); ++tidx) {
-                const Eigen::Vector3i &triangle = mesh_->triangles_[tidx];
-                const Eigen::Vector3d &vertex0 = mesh_->vertices_[triangle(0)];
-                const Eigen::Vector3d &vertex1 = mesh_->vertices_[triangle(1)];
-                const Eigen::Vector3d &vertex2 = mesh_->vertices_[triangle(2)];
+            for (size_t j = 2; j < size; j++) {
+                for (size_t i = 0; i < size - j; i++) {
+                    Weight weight_min =
+                            Weight(std::numeric_limits<double>::max(),
+                                   std::numeric_limits<double>::max());
+                    int weight_index = -1;
+                    int k = i + j;
 
-                Eigen::Vector3d centroid(vertex0.sum() / 3, vertex1.sum() / 3,
-                                         +vertex2.sum() / 3);
+                    for (size_t m = i + 1; m < k; m++) {
+                        Weight weight = minimum_weight[i][m] +
+                                        minimum_weight[m][k] +
+                                        ComputeWeight(i, m, k);
 
-                // Compute the scale attribute as the average length of the
-                // edges that are adjacent to the vertices in the triangle
-                Eigen::Vector3d scale_attribute_vector =
-                        Eigen::Vector3d::Zero();
-                for (size_t vidx = 0; vidx < 3; ++vidx) {
-                    for (int adjacent_vidx :
-                         mesh_->adjacency_list_[triangle(vidx)]) {
-                        const double edge_length =
-                                ComputeVertexToVertexDistance(
-                                        mesh_->vertices_[triangle(vidx)],
-                                        mesh_->vertices_[adjacent_vidx]);
-                        scale_attribute_vector[vidx] += edge_length;
+                        if (weight < weight_min) {
+                            weight_min = weight;
+                            weight_index = m;
+                        }
                     }
-                    scale_attribute_vector[vidx] /=
-                            mesh_->adjacency_list_[triangle(vidx)].size();
-                }
-                double scale_attribute = scale_attribute_vector.mean();
 
-                Eigen::Vector3d scaled_distances = Eigen::Vector3d::Zero();
-                for (size_t vidx = 0; vidx < 3; ++vidx) {
-                    scaled_distances[vidx] =
-                            density_control_factor *
-                            ComputeVertexToVertexDistance(
-                                    centroid, mesh_->vertices_[triangle(vidx)]);
-                }
-
-                if ((scaled_distances[0] > scale_attribute_vector[0] &&
-                     scaled_distances[0] > scale_attribute) ||
-                    (scaled_distances[1] > scale_attribute_vector[1] &&
-                     scaled_distances[1] > scale_attribute) ||
-                    (scaled_distances[2] > scale_attribute_vector[2] &&
-                     scaled_distances[2] > scale_attribute)) {
-                    // Add vertex
-                    int idx_centroid = mesh_->vertices_.size();
-                    mesh_->vertices_.emplace_back(centroid);
-
-                    // Remove existing triangle
-                    mesh_->triangles_.erase(mesh_->triangles_.begin() + tidx);
-
-                    // Add three new triangles
-                    mesh_->triangles_.emplace_back(Eigen::Vector3i(
-                            triangle(0), idx_centroid, triangle(2)));
-                    mesh_->triangles_.emplace_back(Eigen::Vector3i(
-                            triangle(1), idx_centroid, triangle(2)));
-                    mesh_->triangles_.emplace_back(Eigen::Vector3i(
-                            triangle(0), idx_centroid, triangle(1)));
+                    minimum_weight[j][k] = weight_min;
+                    minimum_weight_index[i][k] = weight_index;
                 }
             }
 
-            break;  // TODO: Remove
-
-            // TODO: Find another way to reference the idx. This way won't work,
-            // as the triangles are changed in the previous steps.
-            // first_unrefined_triangle_idx_ += n;
+            AddMinimumWeightTriangulationToMesh(hole, minimum_weight_index, 0,
+                                                size - 1);
         }
     }
 
-    double ComputeVertexToVertexDistance(Eigen::Vector3d &vertex0,
-                                         Eigen::Vector3d &vertex1) {
-        return std::abs((vertex1 - vertex0).sum());
-    }
+    void RefineMesh() { return; }
 
     void FairMesh() { return; }
 
@@ -233,7 +228,6 @@ public:
 private:
     std::shared_ptr<TriangleMesh> mesh_;
     std::vector<std::vector<int>> boundaries_;
-    int first_unrefined_triangle_idx_;
 };
 
 std::shared_ptr<TriangleMesh> TriangleMesh::FillHoles() {
